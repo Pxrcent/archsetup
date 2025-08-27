a186c9a1724f5fef6e461e756c4651f6168aa9f2
Pxrcent <loolzynwoo@gmail.com>
2025-08-27 08:12:28 -0300

    Update Extras.sh

--- Extras.sh
+++ Extras.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo pacman -S kitty nautilus btop fastfetch wayland wayland-utils xorg xorg-server xorg-xinit xf86-video-intel hyprland swaync waybar budgie rofi sddm pipewire networkmanager linux-headers dkms intel-ucode alsa-utils mesa --no-confirm
+sudo pacman -S kitty nautilus btop fastfetch wayland wayland-utils xorg xorg-server xorg-xinit xf86-video-intel hyprland swaync waybar budgie rofi sddm pipewire networkmanager linux-headers dkms intel-ucode alsa-utils mesa --noconfirm
