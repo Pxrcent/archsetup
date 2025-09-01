a405cad0db7230bef551d8561b0e98f8248e963a
Pxrcent <loolzynwoo@gmail.com>
2025-09-01 19:16:41 -0300

    Update Extras.sh

--- Extras.sh
+++ Extras.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo pacman -S kitty nautilus btop fastfetch wayland wayland-utils xorg xorg-server xorg-xinit xf86-video-intel hyprland swaync waybar budgie rofi sddm pipewire networkmanager linux-headers dkms intel-ucode alsa-utils mesa --noconfirm
+sudo pacman -S kitty nautilus btop fastfetch wayland xorg-xwayland wayland-utils xorg xorg-server xorg-xinit xf86-video-intel hyprland swaync waybar rofi pipewire linux-headers dkms intel-ucode alsa-utils mesa --noconfirm
+
+git clone https://aur.archlinux.org/yay.git
+
+cd yay
+
+makepkg -si
