23c353f45d65fa1b2be11e7a30e6194b55242c78
Pxrcent <loolzynwoo@gmail.com>
2025-10-02 15:40:02 -0300

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-sudo pacman -S git nano alacritty nautilus btop fastfetch hyprland swaync waybar wofi pipewire linux-headers dkms mesa otf-font-awesome --noconfirm
+# Do whatever script1 needs to do...
+echo "Running script 1..."
+
+# Create a flag file to signal that this script ran
+touch /tmp/script1.done
+#
+#################################################################
+#FLAG_FILE="/tmp/script1.done"
+#
+# if [ ! -f "$FLAG_FILE" ]; then
+#    echo "Error: script1 must be run before this script."
+#    exit 1
+#fi
+#echo "Script1 was run before — continuing with script2."
+##################################################################
+#
+#
+sudo pacman -S git nano alacritty nautilus btop plocate fastfetch hyprland swaync waybar wofi pipewire networkmanager linux-headers dkms mesa otf-font-awesome nwg-look qt6ct --noconfirm
 
 git clone https://aur.archlinux.org/yay.git
-
 cd yay
-
 makepkg -si
+
